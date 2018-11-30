ba495073b09711c23b165f1594c95dcd4efa0ff9
Kavi Sachania <kavi.sachania@gmail.com>
2018-11-29 19:25:27 -0800

    Heroku setup

--- server/app.js
+++ server/app.js
@@ -45,4 +45,19 @@ app.use(function(err, req, res, next) {
   res.sendStatus(err.status || 500);
 });
 
+
+// express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Resolve client build directory as absolute path to avoid errors in express
+    const path = require('path'); // eslint-disable-line global-require
+    const buildPath = path.resolve(__dirname, '../client/build');
+
+    server.use(express.static(buildPath));
+
+    // // Serve the HTML file included in the CRA client
+    // server.get('/', (request, response) => {
+    //     response.sendFile(path.join(buildPath, 'index.html'));
+    // });
+}
+
 module.exports = app;
